22e5a464246925267dcbca265f20170fb53ca1ff
Jeremy Stoll <j8r8my@gmail.com>
2016-06-22 14:20:31 -0500

    Gen migrated First and last name to User table

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+# This file is an extension to Devise's auth, to include our custom "first_name", "last_name", is_a_coach" and "is_an_admin" columns for the User model.
+#Reference:
+#http://jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html 
+
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :is_a_coach, :is_an_admin, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :is_a_coach, :is_an_admin, :email, :password, :password_confirmation, :current_password)
+  end
+end
