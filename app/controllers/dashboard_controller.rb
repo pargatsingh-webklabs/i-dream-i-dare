0895f0aa66a27cf7ba9389583b8460053bcaa0cb
Jeremy Stoll <j8r8my@gmail.com>
2016-06-25 09:01:47 -0500

    Added hard-coded dashboard with no authentication needed to view as of this point.

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,24 @@
+class DashboardController < ApplicationController
+  skip_before_filter :authenticate_user!, only: [:index]
+
+  def dashboard
+
+    if user_signed_in?
+      redirect_to("/user/dashboard")
+    else redirect_to("/")
+    
+    #TODO: Add Flash error message if redirected to index due to not being logged in.
+    end
+  end
+
+  # # TODO: Insert logic to figure out if user is coach, client, or admin. Create instance variables for each scenario. Refactor. EXAMPLE BELOW: 
+  #   @
+
+  #   @business = current_user.businesses.find_by_id(params[:business_id])
+  #   @clients = @business.clients
+  #   if @business.nil?
+  #     redirect_to("/businesses/view")
+  #   end
+    
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  get "/user/dashboard" => "dashboard#view"
+
   #/////////////////////////////
 
   #devise_for :users, :controllers => { registrations: 'registrations' }
