b279fe9241fe52b999b9362f040d5529781f566e
Jeremy Stoll <j8r8my@gmail.com>
2016-06-27 08:28:34 -0500

    Added About page accessible without sign-in, and relevant route and PagesController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,22 +14,3 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:first_name, :last_name, :is_a_coach, :is_an_admin, :email, :password, :password_confirmation, :current_password)
   end
 end
-
-
-#Below is the first (failing) version of this:
-#
-#
-# class RegistrationsController < Devise::RegistrationsController
-
-#   private
-
-#   def sign_up_params
-#     params.require(:user).permit(:first_name, :last_name, :is_a_coach, :is_an_admin, :email, :password, :password_confirmation)
-#   end
-
-#   def account_update_params
-#     params.require(:user).permit(:first_name, :last_name, :is_a_coach, :is_an_admin, :email, :password, :password_confirmation, :current_password)
-#   end
-# end
-
-#/////////////////////////////////////////////////////////
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
 
   get "/user/dashboard" => "dashboard#view"
 
+  get "/about" => "pages#about"
+
   #/////////////////////////////
 
-  #devise_for :users, :controllers => { registrations: 'registrations' }
+  #Concerning: devise_for :users, :controllers => { registrations: 'registrations' }
 
   # This action from above tells Devise to use my custom registrations_controller.rb in order to accomodate first_name, last_name, is_a_coach, and is_an_admin columns in our User model (http://jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html):
 
