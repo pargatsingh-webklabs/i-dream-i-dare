9edba7934b5e8268a9938e0ab2e5813e224bf86c
Jeremy Stoll <j8r8my@gmail.com>
2016-07-29 12:38:28 -0500

    All filters work for PLANS

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,7 @@
 class PlansController < ApplicationController
 
    before_action :set_plan, only: [:show, :edit, :update, :destroy]
+   before_action :find_mentorship
 
   # /////////////////////////////
 
@@ -64,8 +65,23 @@ class PlansController < ApplicationController
 
   def authorized_user
 
-    # Does the plan belong to the current user?
-    redirect_to "/hit_auth_user_filter" unless @plan.client == current_user.id || @mentorship != nil && @mentorship.coach == current_user.id || current_user.is_an_admin?
+    if @mentorship.present?
+      redirect_to "/hit_auth_user_filter_on_client_check" unless @plan.client == current_user.id
+
+    elsif @coach_mentorships.present?
+
+      @clients_of_coach = []
+
+      @coach_mentorships.each do |m|
+
+        @clients_of_coach << m.client
+
+      end
+
+      redirect_to "/hit_auth_user_filter_on_coach_check" unless @clients_of_coach.include?(@plan.client)
+      
+    else redirect_to "/hit_auth_user_filter_on_admin_check" unless current_user.is_an_admin?
+    end
 
   end
 
@@ -74,7 +90,7 @@ class PlansController < ApplicationController
     # Is the coach of the plan's owner the current user, or is the current user an admin?
     
 
-    if @mentorship != nil
+    if @mentorship.present?
       redirect_to "/hit_auth_coach_filter" unless  @mentorship.coach == current_user.id || current_user.is_an_admin?
     else
       redirect_to "/hit_auth_coach_filter_nil"
@@ -83,11 +99,10 @@ class PlansController < ApplicationController
   end
 
   def admin_user
-
     # Is the current user an admin?
     redirect_to "/hit_auth_admin_filter" unless current_user.is_an_admin == true
-
   end
+
 
 # ////////////////////////////////
 
@@ -99,7 +114,11 @@ class PlansController < ApplicationController
     end
 
     def find_mentorship
-      @mentorship = Mentorship.find_by_client(current_user.id)
+      if current_user.is_a_coach? == false && current_user.is_an_admin? == false
+        @mentorship = Mentorship.find_by(client: current_user.id)
+      elsif current_user.is_a_coach? && current_user.is_an_admin? == false
+        @coach_mentorships = Mentorship.where(coach: current_user.id)
+      end
     end
 
     # Only allow a trusted parameter "white list" through.
