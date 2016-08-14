0f2bbe0f69579cd6d00070285288c62f66fa6897
Jeremy Stoll <j8r8my@gmail.com>
2016-08-14 15:47:34 -0500

    ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.assets.digest = true
   config.log_level = :debug
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
+  # config.action_mailer.smtp_settings = SMTP_SETTINGS
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
