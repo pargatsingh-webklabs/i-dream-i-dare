2046c9579755209b2563c5ac26a104808dfa8dd6
Jeremy Stoll <j8r8my@gmail.com>
2016-08-14 14:33:35 -0500

    env

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,14 +1,26 @@
 SMTP_SETTINGS = {
 
+  # PREVIOUS SETTINGS:
+
   # address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
+  # authentication: :plain,
+  # domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
+  # enable_starttls_auto: true,
+  # password: ENV.fetch("SMTP_PASSWORD"),
+  # port: "587",
+  # user_name: ENV.fetch("SMTP_USERNAME")
+
+  # ////////////////////////////////
 
   
-address: "smtp.sendgrid.net",
-
+  address: "smtp.sendgrid.net",
+  # example: "smtp.sendgrid.net"
   authentication: :plain,
-  domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
+  domain: "heroku.com", 
+  # example: "heroku.com"
   enable_starttls_auto: true,
-  password: ENV.fetch("SMTP_PASSWORD"),
+  password: "password",
   port: "587",
-  user_name: ENV.fetch("SMTP_USERNAME")
+  user_name: "username"
+  
 }
