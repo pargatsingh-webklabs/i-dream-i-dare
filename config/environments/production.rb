3ba97d90b70d5fe1c0b8b142e49ed11bff22a15b
Jeremy Stoll <j8r8my@gmail.com>
2018-06-04 22:04:46 -0500

    Deploy fixes 07

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.middleware.use Rack::Deflater
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.log_level = :debug
   config.action_mailer.delivery_method = :smtp
