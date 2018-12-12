d4bd8bd86adb73e49b0a7f11226dada64be17aa7
Jeremy Stoll <j8r8my@gmail.com>
2018-12-11 20:10:29 -0600

    Header update to allow iFrame from idreamidare.com

--- app/controllers/concerns/webhookable.rb
+++ app/controllers/concerns/webhookable.rb
@@ -3,6 +3,7 @@ extend ActiveSupport::Concern
  
   def set_header
           response.headers["Content-Type"] = "text/xml"
+          response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://idreamidare.com"
   end
  
   def render_twiml(response)
