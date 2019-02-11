b8b528be02bf3e75c863fdb276ab69a32d7a73d8
jeremystoll <j8r8my@gmail.com>
2019-02-11 15:12:22 -0600

    Disabled action mailer host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8" 
-  config.action_mailer.default_url_options = { :host => 'http://206.81.14.195/i-dream-i-dare-master' }
+  # config.action_mailer.default_url_options = { :host => 'http://206.81.14.195/i-dream-i-dare-master' }
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
