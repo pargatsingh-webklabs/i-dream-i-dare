38badc5434ca746c9ac9054258935c0a44a3ecf4
jeremystoll <j8r8my@gmail.com>
2019-02-11 14:21:29 -0600

    Updates to env variables

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,9 +1,9 @@
 require_relative "production"
 
-Mail.register_interceptor(
-  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
-)
-ActiveModelSerializers.config.adapter = :json_api
+# Mail.register_interceptor(
+#   RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
+# )
+# ActiveModelSerializers.config.adapter = :json_api
 Rails.application.configure do
   # ...
 end
