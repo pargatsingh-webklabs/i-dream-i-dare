e8518c2df5a4f2089e24228c14b00ba6194258af
jeremystoll <j8r8my@gmail.com>
2019-02-11 14:46:18 -0600

    Updated Create Schedule migration

--- db/migrate/20190205033235_create_user_schedules.rb
+++ db/migrate/20190205033235_create_user_schedules.rb
@@ -1,8 +1,8 @@
 class CreateUserSchedules < ActiveRecord::Migration
   def change
     create_table :user_schedules do |t|
-      t.int :user_id
-      t.int :schedule_id
+      t.integer :user_id
+      t.integer :schedule_id
       t.boolean :completed
 
       t.timestamps null: false
