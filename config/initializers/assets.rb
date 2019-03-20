c470d85dad9f99cffcb6d04f062959b5985c586d
jeremystoll <j8r8my@gmail.com>
2019-03-19 21:43:19 -0500

    Merging of SEO-work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( trix_attachments.js )
+
+Rails.application.config.assets.precompile += %w( custom.js )
